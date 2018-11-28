cfbccf22cc5fbffde0fa06757c129b09d9711a19
JoeVincentt <eugene.bb@hotmail.com>
2018-11-27 20:43:23 -0500

    creating todos

--- server/index.js
+++ server/index.js
@@ -16,6 +16,7 @@ const Todo = mongoose.model("Todo", {
 const typeDefs = `
   type Query {
     hello(name: String): String!
+    todos: [Todo]
   }
   type Todo {
     id: ID!
@@ -29,7 +30,8 @@ const typeDefs = `
 
 const resolvers = {
   Query: {
-    hello: (_, { name }) => `Hello ${name || "World"}`
+    hello: (_, { name }) => `Hello ${name || "World"}`,
+    todos: () => Todo.find()
   },
   Mutation: {
     createTodo: async (_, { text }) => {
