28c2736a360024399f54b6f584b943be7121a8ab
JoeVincentt <eugene.bb@hotmail.com>
2018-11-27 20:34:51 -0500

    setup server create table connect mongooseDB

--- server/index.js
+++ server/index.js
@@ -1,18 +1,47 @@
-import { GraphQLServer } from "graphql-yoga";
-// ... or using `require()`
-// const { GraphQLServer } = require('graphql-yoga')
+// import { GraphQLServer } from "graphql-yoga";
+// // ... or using `require()`
+const { GraphQLServer } = require("graphql-yoga");
+const mongoose = require("mongoose");
+
+mongoose.connect(
+  "mongodb://localhost/test",
+  { useNewUrlParser: true }
+);
+
+const Todo = mongoose.model("Todo", {
+  text: String,
+  complete: Boolean
+});
 
 const typeDefs = `
   type Query {
     hello(name: String): String!
+  }
+  type Todo {
+    id: ID!
+    text: String!
+    complete: Boolean!
+  }
+  type Mutation {
+    createTodo(text: String!): Todo
   }
 `;
 
 const resolvers = {
   Query: {
     hello: (_, { name }) => `Hello ${name || "World"}`
+  },
+  Mutation: {
+    createTodo: async (_, { text }) => {
+      const todo = new Todo({ text, complete: false });
+      await todo.save();
+      return todo;
+    }
   }
 };
 
 const server = new GraphQLServer({ typeDefs, resolvers });
-server.start(() => console.log("Server is running on localhost:4000"));
+mongoose.connection.once("open", function() {
+  // we're connected!
+  server.start(() => console.log("Server is running on localhost:4000"));
+});
